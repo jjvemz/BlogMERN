e7cc872a0d8101dec8f9dbe6e1e5b1dff6ee3e70
juan.vergaram@mail.udp.cl <juan.vergaram@mail.udp.cl>
2023-10-03 16:06:28 -0300

    userControllers and Models complete!

---
+++ Backend/Controllers/userController.js
@@ -0,0 +1,69 @@
+import User from '../Models/User';
+import bcrypt from "bcrypt"
+
+export const getAlluser = async (req, res, next) =>{
+    let users;
+    try{
+        users = await User.find();
+    }catch(error){
+        return console.log(error);
+    }
+
+    if(!users){
+        return res.status(404).json({message:"User not found!"});
+    }
+    return res.status(200).json({users});
+};
+
+export const signUp = async(req, res, next)=>{
+    const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return console.log(error);
+  }
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "User Already exists! Login Instead" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
+    blogs: [],
+  });
+
+  try {
+    await user.save();
+  } catch (error) {
+    return console.log(error);
+  }
+
+  return res.status(201).json({ user });
+};
+export const Login = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    let existingUser;
+    try {
+      existingUser = await User.findOne({ email });
+    } catch (error) {
+      return console.log(error);
+    }
+    if (!existingUser) {
+      return res.status(404).json({ message: "User Not Found! Register First" });
+    }
+  
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Incorrect Password!" });
+    }
+    return res
+      .status(200)
+      .json({ message: "Login Successful!!!", user: existingUser });
+  };

edd3c5c8158217ea72b20e55614edcc751efe4d5
juan.vergaram@mail.udp.cl <juan.vergaram@mail.udp.cl>
2023-10-12 20:02:34 -0300

    FrontEnd initialized

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import postRouter from "./Routes/postRoutes";
+const routes = require("./Routes");
 
 dotenv.config();
 const app = express();
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/v1/api/blog", postRouter);
+app.use(routes)
 
 mongoose
   .connect(process.env.DATABASE_URL)
